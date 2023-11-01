a26483bfc800c677b2b7d96bdbaea6b63f881476
AnhNguyen <anh.nguyen@aitechasia.vn>
2023-11-01 10:51:27 +0700

    13123

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,45 @@
+"use client";
+import React from "react";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  const [products, setProducts] = React.useState<any[]>([]);
+  const fetchData = async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/products?populate=*`,
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    const product = await res.json();
+    console.log(product.data);
+    setProducts(product.data);
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div style={{textAlign:'center',paddingBottom:24}}>
+      Test deploy
+      <div
+        style={{ padding: 8, display: "flex", justifyContent: "space-between" }}
+      >
+        {products.map((product, idx) => {
+          return (
+            <div key={idx}>
+              {product.attributes.name} -{" "}
+              <img
+                src={`${product.attributes.image?.data.attributes.url}`}
+                width={50}
+                height={50}
+                alt="no img"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
